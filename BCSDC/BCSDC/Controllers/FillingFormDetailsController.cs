923734c28007e4147230624d8a6cf2af49741f67
Pankaj07102019 <74609723+Pankaj07102019@users.noreply.github.com>
2021-01-04 22:09:57 +0530

    Created Filling forms details page and their supporting classes

---
+++ BCSDC/BCSDC/Controllers/FillingFormDetailsController.cs
@@ -0,0 +1,85 @@
+﻿using BCSDC.DAL;
+using BCSDC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BCSDC.Controllers
+{
+    public class FillingFormDetailsController : Controller
+    {
+        // GET: FillingFormDetails
+        public ActionResult FillingFormDetails(string Form_Name)
+        {
+            FormPreview FormPreviewModel = new FormPreview();
+            //string FName = (string)Session["FormName"];
+            var ReturnData = SearchDAL.SearchForms(Form_Name, "", "", "");
+            List<FormControlsList> lstcnt = new List<FormControlsList>();
+            foreach (var Items in ReturnData)
+            {
+                FormPreviewModel.FormName = Items.Form_Name;
+                lstcnt.Add(new FormControlsList
+                {
+                    FieldName = Items.Field_Name.ToString(),
+                    FieldType = Items.Field_Type.ToString(),
+                    FieldValue = Items.Field_Value.ToString()
+                });
+            }
+            FormPreviewModel.lstControls = lstcnt;
+            return View("~/Views/FillingFormDetails/FillingFormDetails.cshtml", FormPreviewModel);
+        }
+        
+        [HttpPost]
+        public ActionResult DeleteForms(string Form_Name)
+        {
+            return View("FillingFormDetails");
+        }
+
+        public ActionResult FillFormDetails()
+        {
+            FormPreview FormPreviewModel = new FormPreview();
+            string FName = (string) Session["FormName"];
+            var ReturnData = SearchDAL.SearchForms(FName, "", "", "");
+            List<FormControlsList> lstcnt = new List<FormControlsList>();
+            foreach (var Items in ReturnData)
+            {
+                FormPreviewModel.FormName = Items.Form_Name;
+                lstcnt.Add(new FormControlsList
+                {
+                    FieldName = Items.Field_Name.ToString(),
+                    FieldType = Items.Field_Type.ToString(),
+                    FieldValue = Items.Field_Value.ToString()
+                });
+            }
+            FormPreviewModel.lstControls = lstcnt;
+            return View("~/Views/FillingFormDetails/FillingFormDetails.cshtml", FormPreviewModel);
+        }
+        public void RedirectToFormsDetails(string Form_Name)
+        {
+            Session["FormName"] = Form_Name;
+            RedirectToAction("FillFormDetails");
+        }
+
+        public ActionResult SaveDetails()
+        {
+            FormPreview FormPreviewModel = new FormPreview();
+            string FName = (string)Session["FormName"];
+            var ReturnData = SearchDAL.SearchForms(FName, "", "", "");
+            List<FormControlsList> lstcnt = new List<FormControlsList>();
+            foreach (var Items in ReturnData)
+            {
+                FormPreviewModel.FormName = Items.Form_Name;
+                lstcnt.Add(new FormControlsList
+                {
+                    FieldName = Items.Field_Name.ToString(),
+                    FieldType = Items.Field_Type.ToString(),
+                    FieldValue = Items.Field_Value.ToString()
+                });
+            }
+            FormPreviewModel.lstControls = lstcnt;
+            return View("~/Views/FillingFormDetails/FillingFormDetails.cshtml", FormPreviewModel);
+        }
+    }
+}
