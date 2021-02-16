d0df750bde8e4f2882967d57a3880dda67cd08f6
Pankaj07102019 <74609723+Pankaj07102019@users.noreply.github.com>
2021-02-16 20:43:16 +0530

    UAT bug fixes

--- BCSDC/BCSDC/App_Start/RouteConfig.cs
+++ BCSDC/BCSDC/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace BCSDC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+              name: "Login",
+              url: "DigitalForms/Login",
+              defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
